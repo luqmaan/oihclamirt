82fc53a6a9008a37b7bc13787c8515a7c8ac57ca
Luqmaan <ldawoodjee@gmail.com>
2017-09-03 18:06:54 -0500

    Add parallel example

---
+++ parallel-example.js
@@ -0,0 +1,39 @@
+// Parallel example for @sixsneakers
+//
+// How do you keep scraping a bunch of sites non-stop all at once?
+// Here's one way to do it.
+
+const { meow } = require('./scraper');
+
+const searches = [
+  {
+    keywords: ['melange'],
+    exclude: ['HOODIE'],
+    sizes: ['*'],
+  },
+];
+
+const slackToken = 'https://hooks.slack.com/services/BLAH';
+
+const sites = [
+  { oembed: 'https://yeezysupply.com/collections/all.oembed' },
+  { atom: 'https://shopnicekicks.com/collections/all.atom' },
+];
+
+function delay(ms) {
+  return new Promise(resolve => {
+    setTimeout(resolve, ms);
+  })
+}
+
+async function scrapeSiteInfinitely(site) {
+  console.log('Starting scraping site:', site);
+  await meow(searches, site.oembed, site.atom, slackToken);
+  console.log('Done scraping site:', site);
+  const randomDelaySeconds = Math.max(3000, Math.random() * 15000);
+  console.log(`Delaying for ${randomDelaySeconds / 1000} seconds`);
+  await delay(randomDelaySeconds);
+  scrapeSiteInfinitely(site);
+}
+
+sites.map(site => scrapeSiteInfinitely(site));
